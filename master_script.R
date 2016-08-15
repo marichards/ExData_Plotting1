d7914fa708f8360abfffe66607999bfc2b064170
marichards <marichards88@gmail.com>
2016-08-14 17:36:47 -0700

    Update the master script

--- master_script.R
+++ master_script.R
@@ -1,6 +1,13 @@
+#########################################
+##
+## master_script.R
+##
 ## Master sourcing script
-
 ## Runs all the other plotting scripts
+##
+## Matthew Richards
+#########################################
+
 
 source("C:\\Users\\Matt\\Documents\\GitHub\\ExData_Plotting1\\plot1.R")
 source("C:\\Users\\Matt\\Documents\\GitHub\\ExData_Plotting1\\plot2.R")
